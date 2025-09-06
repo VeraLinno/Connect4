9f594cacadd853469d2e1b3591c02176a78e73ca
Vera Linno <velinn@taltech.ee>
2025-09-06 21:20:35 +0300

    .

---
+++ hyper-connectx/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Library1;
+
+Console.WriteLine("Hello, World!");
+
+FizzBuzzLogic.FizzBuzzInstructions();
+var countTo = FizzBuzzLogic.GetFizzBuzzCount();
+FizzBuzzLogic.FizzBuzz(countTo);
---
+++ hyper-connectx/Library1/Class1.cs
@@ -0,0 +1,60 @@
+﻿namespace Library1;
+
+public static class FizzBuzzLogic
+{
+    public static void FizzBuzzInstructions()
+    {
+        Console.WriteLine("Fizz buzz is a group word game for children to teach them about division." +
+                          "[1] Players take turns to count incrementally, replacing any number divisible by three with the word \"fizz\"," +
+                          " and any number divisible by five with the word \"buzz\", " +
+                          "and any number divisible by both three and five with the word \"fizzbuzz\".");
+    }
+
+    public static int GetFizzBuzzCount() 
+    {
+        Console.Write("Enter a number to start: ");
+        var userInput = Console.ReadLine();
+        
+        var countTo = 100;
+
+        if (userInput != null)
+        {
+            if(!int.TryParse(userInput,  out countTo))
+            {
+                countTo = 100;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a number greater than zero.");
+        }
+        return countTo;
+    }
+
+    public static void FizzBuzz(int number)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("fizz");
+
+                if (i % 5 == 0)
+                {
+                    Console.Write("buzz");
+                }
+
+                Console.Write(", ");
+                continue;
+            }
+
+            if (i % 5 == 0)
+            {
+                Console.Write("buzz, ");
+                continue;
+            }
+
+            Console.Write(i + ", ");
+        }
+    }
+}
