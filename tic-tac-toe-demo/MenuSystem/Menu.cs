409ac2bbc3143c801a4b3d3f835c5051b8cb4693
Vera Linno <velinn@taltech.ee>
2025-09-12 15:24:03 +0300

    part1

---
+++ tic-tac-toe-demo/MenuSystem/Menu.cs
@@ -0,0 +1,51 @@
+﻿namespace MenuSystem;
+
+public class Menu
+{
+    private List<MenuItem> MenuItems { get; set; } = new ();
+
+    public void AddMenuItems(List<MenuItem> items)
+    {
+        // check for validity
+        //add
+
+        foreach (var item in items)
+        {
+            // control (dict)
+            
+            MenuItems.Add(item);
+        }
+    }
+
+    public void Run() // or public string run() , then you need to return smth
+    {
+        var menuIsDone = false;
+        do
+        {
+            // display menu
+            // get user input
+            // validate 
+            // execute choice
+            // loop back to top
+            
+            DisplayMenu();
+            Console.Write("Please enter your choice: ");
+            var userInput = Console.ReadLine();
+
+            if (userInput == "X")
+            {
+                menuIsDone = true;
+            }
+                
+        } while (!menuIsDone);
+        
+    }
+
+    private void DisplayMenu()
+    {
+        foreach (var item in MenuItems)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
