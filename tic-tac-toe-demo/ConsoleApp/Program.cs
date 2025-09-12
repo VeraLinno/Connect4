daa956ce52dc4ac421bd09d7021ec8b3e8acc8b8
Vera Linno <velinn@taltech.ee>
2025-09-12 15:25:54 +0300

    part1

---
+++ tic-tac-toe-demo/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+using MenuSystem;
+
+Console.WriteLine("Hello, TIC-TAC-TOE");
+
+var mainMenu = new Menu();
+
+mainMenu.AddMenuItems(
+    [
+            new MenuItem
+            {
+                Label = "Label 1",
+                Key = "1"
+            },
+            new MenuItem
+            {
+                Label = "Label 2",
+                Key = "2" 
+            } 
+    ]);
+
+mainMenu.Run();
---
+++ tic-tac-toe-demo/MenuSystem/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MenuSystem;
+
+public class MenuItem
+{
+    public string Label { get; set; }
+    public string Key{ get; set; }
+    // thing that should happen...
+    
+    public override string ToString() => $"{Key}) {Label}";
+}
