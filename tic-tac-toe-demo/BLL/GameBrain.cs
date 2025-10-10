fc07e77942c3b402c092ebc53ab113a6a09cc277
Vera Linno <velinn@taltech.ee>
2025-10-10 09:47:16 +0300

    loeng 5

---
+++ tic-tac-toe-demo/BLL/GameBrain.cs
@@ -0,0 +1,107 @@
+﻿namespace BLL;
+
+public class GameBrain
+{
+    private EBoardState[,] GameBoard { get; set; }
+    private GameConfiguration GameConfiguration { get; set; }
+    private string Player1Name { get; set; }
+    private string Player2Name { get; set; }
+
+    private bool NextMoveByX { get; set; } = true;
+
+    private Random random = new Random();
+
+    public GameBrain(GameConfiguration configuration, string player1Name, string player2Name)
+    {
+        GameConfiguration = configuration;
+        Player1Name = player1Name;
+        Player2Name = player2Name;
+        GameBoard = new EBoardState[configuration.BoardWidth, configuration.BoardHeight];
+    }
+
+    public EBoardState[,] GetBoard()
+    {
+        var gameBoardCopy = new EBoardState[GameConfiguration.BoardWidth, GameConfiguration.BoardHeight];
+        Array.Copy(GameBoard, gameBoardCopy, GameBoard.Length);
+        return gameBoardCopy;
+    }
+
+    public bool IsNextPlayerX() => NextMoveByX;
+
+
+    public void ProcessMove(int x, int y)
+    {
+        if (GameBoard[x, y] == EBoardState.Empty)
+        {
+            GameBoard[x, y] = NextMoveByX ? EBoardState.X : EBoardState.O;
+            NextMoveByX = !NextMoveByX;
+        }
+    }
+
+    private (int dirX, int dirY) GetDirection(int directionIndex) =>
+        directionIndex switch
+        {
+            0 => (-1, -1), // Diagonal up-left
+            1 => (0, -1), // Vertical
+            2 => (1, -1), // Diagonal up-right
+            3 => (1, 0), // horizontal
+            _ => (0, 0)
+        };
+
+    private (int dirX, int dirY) FlipDirection((int dirX, int dirY) direction) =>
+        (-direction.dirX, -direction.dirY);
+
+    public bool BoardCoordinatesAreValid(int x, int y)
+    {
+        if (x < 0 || x >= (GameConfiguration.BoardWidth - 1)) return false;
+        if (y < 0 || y >= (GameConfiguration.BoardHeight - 1)) return false;
+        return true;
+    }
+
+    public EBoardState GetWinner(int x, int y)
+    {
+        if (GameBoard[x, y] == EBoardState.Empty) return EBoardState.Empty;
+
+        
+
+        for (int directionIndex = 0; directionIndex < 4; directionIndex++)
+        {
+            var (dirX, dirY) = GetDirection(directionIndex);
+
+            var count = 0;
+            
+            var nextX = x;
+            var nextY = y;
+            while (BoardCoordinatesAreValid(nextX, nextY) && GameBoard[x, y] == GameBoard[nextX, nextY] &&
+                   count <= GameConfiguration.WinCondition)
+            {
+                count++;
+                nextX += dirX;
+                nextY += dirY;
+            }
+
+            if (count < GameConfiguration.WinCondition)
+            {
+                (dirX, dirY) = FlipDirection((dirX, dirY));
+                nextX = x + dirX;
+                nextY = y + dirY;
+                while (BoardCoordinatesAreValid(nextX, nextY) && GameBoard[x, y] == GameBoard[nextX, nextY] &&
+                       count <= GameConfiguration.WinCondition)
+                {
+                    count++;
+                    nextX += dirX;
+                    nextY += dirY;
+                }
+            }
+            
+            if (count == GameConfiguration.WinCondition)
+            {
+                return GameBoard[x, y] == EBoardState.X ? EBoardState.XWin : EBoardState.OWin;
+            }
+
+        }
+
+
+        return EBoardState.Empty;
+    }
+}
---
+++ tic-tac-toe-demo/ConsoleUI/Ui.cs
@@ -0,0 +1,52 @@
+﻿using BLL;
+
+namespace ConsoleUI;
+
+public static class Ui 
+{
+    public static void ShowNextPlayer(bool isNextPlayer)
+    {
+        Console.WriteLine("Next player:" + (isNextPlayer ? "X" : "O"));
+    }
+    
+    public static void LoadBoard(EBoardState[, ]  gameBoard)
+    {
+        Console.Write("   ");
+        for (int x = 0; x < gameBoard.GetLength(0); x++)
+        {
+            Console.Write("|" + GetNumberRepresentation(x + 1));
+        }
+        Console.WriteLine(); 
+        
+        for (int y = 0; y < gameBoard.GetLength(1); y++) 
+        {
+            for (int x = 0; x < gameBoard.GetLength(0); x++) {
+                Console.Write("---+");            
+            }
+            Console.WriteLine("---");
+           
+            Console.Write(GetNumberRepresentation(y + 1));
+            for (int x = 0; x < gameBoard.GetLength(0); x++)
+            {
+                Console.Write("|" + GetCellRepresentation(gameBoard[x, y]));
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static string GetNumberRepresentation(int number)
+    {
+        return " " + (number < 10 ? "0" + number : number.ToString());
+    }
+
+    private static string GetCellRepresentation(EBoardState cellValue) =>
+        cellValue switch
+        {
+            EBoardState.Empty => "   ",
+            EBoardState.X => " X ",
+            EBoardState.O => " O ",
+            EBoardState.XWin => "XXX",
+            EBoardState.OWin => "OOO",
+            _ => " ? "
+        };
+}
